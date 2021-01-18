c3725e8064ef41212b47ab22950fea7b4a0d51cd
Tomoharu Watanabe <watatomo1216@gmail.com>
2021-01-19 02:56:22 +0900

    BlogListを実装

--- src/router/routes.js
+++ src/router/routes.js
@@ -3,6 +3,10 @@ const routes = [
     path: '/',
     name: 'Home',
   },
+  {
+    path: '/blog-list',
+    name: 'BlogList',
+  },
 ]
 
 export default routes
---
+++ src/views/BlogList.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="blog-list">
+    <v-container class="grey lighten-5">
+      <v-row v-for="n in 5" :key="n">
+        <v-col>
+          <BlogCard />
+        </v-col>
+      </v-row>
+    </v-container>
+  </div>
+</template>
+
+<script>
+import BlogCard from '../components/BlogCard'
+
+export default {
+  components: {
+    BlogCard
+  }
+}
+</script>
