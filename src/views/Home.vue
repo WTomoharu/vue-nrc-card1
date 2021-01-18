ead5220f12e19cb24aa875ba5747f356f0601678
Tomoharu Watanabe <watatomo1216@gmail.com>
2021-01-19 05:03:09 +0900

    Homeに各ページへのリンクを追加

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,5 +1,33 @@
 <template>
-  <div class="home">
-    <h1>This is an Home page</h1>
+  <div
+    class="home"
+    style="margin: 16px"
+  >
+    <div>
+      N高鉄道同好会のデモサイトです。<br>
+      左上のメニュー、または以下のリンクから<br>
+      各デモページに行くことができます。
+    </div>
+
+    <div style="height: 16px" />
+
+    <div
+      v-for="route in routes.slice(1)"
+      :key="route.path"
+    >
+      <router-link
+        :to="route.path"
+      >
+        {{ route.ja }}（{{ route.path }}）
+      </router-link>
+    </div>
   </div>
 </template>
+
+<script>
+import routes from '../router/routes'
+
+export default {
+  data() { return { routes } }
+}
+</script>
