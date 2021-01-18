d99f8a0642984a288ea4dc18ea7bb10ea9e8f07f
Tomoharu Watanabe <watatomo1216@gmail.com>
2021-01-19 04:27:56 +0900

    NavigationDrawerを実装

--- src/App.vue
+++ src/App.vue
@@ -5,12 +5,30 @@
       color="primary"
       dark
     >
+      <v-app-bar-nav-icon
+        @click.stop="drawer = !drawer"
+      />
+
       <v-toolbar-title>
         N HighSchool Railway Club
       </v-toolbar-title>
-
-      <v-spacer/>
     </v-app-bar>
+
+    <v-navigation-drawer v-model="drawer" app>
+      <v-list nav dense>
+        <v-list-item-group>
+          <v-list-item
+            v-for="route in routes"
+            :key="route.path"
+            :to="route.path"
+          >
+            <v-list-item-title>
+              {{ route.name }}
+              </v-list-item-title>
+          </v-list-item>
+        </v-list-item-group>
+      </v-list>
+    </v-navigation-drawer>
 
     <v-main>
       <router-view/>
@@ -19,7 +37,15 @@
 </template>
 
 <script>
+import routes from './router/routes'
+
 export default {
-  name: 'App'
+  name: 'App',
+  data() {
+    return {
+      drawer: false,
+      routes
+    }
+  }
 }
 </script>
